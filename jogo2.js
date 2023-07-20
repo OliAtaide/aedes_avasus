42d58d8723df1dc25cf8dadc707c55d3e52c5d4e
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2023-07-20 13:59:32 -0300

    Colocando e removendo grupos nos criadouros.

--- jogo2.js
+++ jogo2.js
@@ -1,5 +1,12 @@
 var criadouros = [];
 
+function imprimirGrupo(index, letra) {
+    return ('<div data-index=' + index +
+        ' data-value=' + letra +
+        ' class="btn btn-grupo">'
+        + letra + '</div>')
+}
+
 function printQuestoes() {
     $.ajax({
         url: './aedes.json',
@@ -8,16 +15,15 @@ function printQuestoes() {
         success: function (_data) {
             criadouros = _data[1].criadouros;
             _data[1].grupos.forEach(function (value, i) {
+                var content = '';
+                console.log(value.quantidade)
+                for (let j = 0; j < value.quantidade; j++) {
+                    content += imprimirGrupo(i, value.letra)
+                }
                 $('.row-grupos').append(
-                    `
-                    <div class="col-6">
-                        <div data-index=` + i + ` data-value=` + value.letra + ` class="btn btn-grupo">
-                            `
-                    + value.letra +
-                    `
-                        </div>
-                    </div>
-                    `
+                    '<div class="col-6 col-grupo col-grupo-' + value.letra + '">'
+                    + content +
+                    '</div>'
                 )
             })
             $('.btn-grupo').draggable();
@@ -25,15 +31,17 @@ function printQuestoes() {
             _data[1].criadouros.forEach(function (value, i) {
                 $('.row-criadouros').append(
                     `
-                    <div class="col-3">
-                        <div class="d-flex flex-column justify-content-center">
+                    <div class="col-3 col-criadouro">
+                        <div class="flex-criadouro d-flex flex-column justify-content-center">
                             <p class="text-center">`
                     + value.titulo +
                     `
                             </p>
+                            <div class="close-container">
+                            </div>
                             <input data-index=`
                     + i +
-                    ` class="input-criadouro" value="A" disabled>
+                    ` class="input-criadouro" disabled>
                         </div>
                     </div>
                     `
@@ -57,6 +65,16 @@ function printQuestoes() {
         },
     })
 }
+
+$(document).on('click', '.close-container', function () {
+    var input = $(this).parent().find('input');
+    console.log($('.col-grupo-' + input.val()));
+    $('.col-grupo-' + input.val()).append(
+        imprimirGrupo(input.data('index'), input.val())
+    )
+    input.val('');
+    input.removeClass('ui-dropped');
+})
 
 printQuestoes();
 
