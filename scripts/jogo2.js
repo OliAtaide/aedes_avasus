5c0f76ecd93a5a6084b6c1990ce7c783cbe05298
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2023-08-09 13:40:42 -0300

    Responsividade Jogo 2

--- scripts/jogo2.js
+++ scripts/jogo2.js
@@ -1,5 +1,6 @@
 var criadouros = [];
 var grupos = [];
+var current_index = -1;
 
 function imprimirGrupo(index, letra) {
     return ('<div data-index=' + index +
@@ -22,7 +23,7 @@ function printQuestoes() {
                     content += imprimirGrupo(i, value.letra)
                 }
                 $('.row-grupos').append(
-                    '<div class="col-6 col-grupo col-grupo-' + value.letra + '">'
+                    '<div class=" col-3 col-sm-6 col-grupo col-grupo-' + value.letra + '">'
                     + content +
                     '</div>'
                 )
@@ -32,17 +33,17 @@ function printQuestoes() {
             _data[1].criadouros.forEach(function (value, i) {
                 $('.row-criadouros').prepend(
                     `
-                    <div class="col-3 col-criadouro">
+                    <div class="col-6 col-sm-3 col-criadouro">
                         <div class="flex-criadouro d-flex flex-column justify-content-center">
-                            <p class="text-center my-2">`
+                            <div class="d-flex align-items-end mt-auto"><p class="text-center my-2 w-100">`
                     + value.titulo +
                     `
-                            </p>
+                            </p></div>
                             <div class="close-container">
                             </div>
-                            <input data-index=`
+                            <button data-index=`
                     + i +
-                    ` class="input-criadouro" disabled>
+                    ` class="input-criadouro" ></button>
                         </div>
                     </div>
                     `
@@ -60,6 +61,7 @@ function printQuestoes() {
                         $('.btn-grupo-' + $(this).val()).draggable();
                     }
                     $(this).val(ui.draggable.data("value"));
+                    $(this).html(ui.draggable.data("value"));
                     $(this).addClass('ui-dropped')
                     ui.draggable.remove()
                 }
@@ -72,6 +74,7 @@ function printQuestoes() {
     })
 }
 
+
 $(document).on('click', '.close-container', function () {
     var input = $(this).parent().find('input');
     $('.col-grupo-' + input.val()).append(
@@ -83,6 +86,26 @@ $(document).on('click', '.close-container', function () {
 })
 
 printQuestoes();
+
+$(document).on('click', '.btn-grupo', function () {
+    current_index = $(this).data('index');
+    console.log(current_index)
+})
+
+$(document).on('click', '.input-criadouro', function () {
+    if ($(this).hasClass('ui-dropped')) {
+        $('.col-grupo-' + $(this).val()).append(
+            imprimirGrupo($(this).data('index'), $(this).val())
+        )
+        $('.btn-grupo-' + $(this).val()).draggable();
+    }
+    const current_btn = $('.btn-grupo[data-index= ' + current_index +']').eq(0);
+    console.log(current_btn);
+    $(this).val(current_btn.data("value"));
+    $(this).html(current_btn.data("value"));
+    $(this).addClass('ui-dropped')
+    current_btn.remove()
+})
 
 $(document).on('click', '.card-feedback .btn-back', function () {
     $('.card-feedback').hide();
@@ -103,7 +126,7 @@ $(document).on('click', '.btn-verif', function () {
     var acertos = 0;
     if ($('.ui-dropped').length == criadouros.length) {
         $('.ui-dropped').each(function (value, i) {
-            
+
             var criadouro = criadouros[$(this).data('index')]
             if (criadouro.resposta == $(this).val()) {
                 $(this).addClass('ui-right');
@@ -118,7 +141,7 @@ $(document).on('click', '.btn-verif', function () {
         )
         success.show();
     }
-    
+
     //$('.btn-verif').hide();
     //$('.btn-retry').show();
     //}
