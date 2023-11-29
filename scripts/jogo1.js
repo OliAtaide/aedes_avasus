7ddd39b74df9197eff74d0662a8aa40dc1b718af
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2023-11-29 10:01:50 -0300

    Botoes de Refazer e finalizar no jogo 2

--- scripts/jogo1.js
+++ scripts/jogo1.js
@@ -1,16 +1,17 @@
-var questoes;
+let questoes;
 var acertos = 0;
+var acertou = false;
 
 function printQuestoes(num) {
-    $.ajax({
-        url: '../scripts/aedes.json',
-        dataType: "json",
-        type: 'GET',
-        success: function (_data) {
-            questoes = _data[num].questoes;
-            _data[num].questoes.forEach(function (value, i) {
-                $('.game-body').append(
-                    `
+  $.ajax({
+    url: "../scripts/aedes.json",
+    dataType: "json",
+    type: "GET",
+    success: function (_data) {
+      questoes = _data[num].questoes;
+      _data[num].questoes.forEach(function (value, i) {
+        $(".game-body").append(
+          `
                         <div style="display: none;" class="card game-card game-card-${i} m-auto">
                             <h4 class="card-title text-center my-auto"> 
                                 ${value.titulo}
@@ -30,29 +31,33 @@ function printQuestoes(num) {
                         </div>
                         <div style="display: none;" class="card game-card game-card-${i}-result m-auto" >
                             <h4 class="card-title text-center my-auto">
-                                ${value.verdade ? 'VERDADE' : 'MITO  '}
+                                ${value.verdade ? "VERDADE" : "MITO  "}
                                 <br><br>
                                 ${value.texto}
                             </h4>
                             <div class="row">
                                 <div class="col">
-                                    <button class="btn btn-back w-100">
+                                    <button class="btn btn-back w-100" data-current="game-card-${i}">
                                         Voltar
                                     </button>
                                 </div>
                                 <div class="col">
-                                    <button class="btn btn-next w-100" data-next="game-card-${i + 1}">
+                                    <button class="btn btn-next w-100" data-next="game-card-${
+                                      i + 1
+                                    }">
                                         Próxima Pergunta
                                     </button>
                                 </div>
                             </div>
                         </div>
                         `
-                )
-                if (i == questoes.length - 1) {
-                    $('.game-body').append(
-                        `
-                            <div style="display: none;" class="card game-card game-card-${i + 1} p-3 m-auto" >
+        );
+        if (i == questoes.length - 1) {
+          $(".game-body").append(
+            `
+                            <div style="display: none;" class="card game-card game-card-${
+                              i + 1
+                            } p-3 m-auto" >
                                 <h4 class="card-title text-center my-auto">
                                     VOCÊ COMPLETOU!
                                     <br><br>
@@ -60,47 +65,63 @@ function printQuestoes(num) {
                                 </h4>
                                 <div class="row">
                                     <div class="col">
-                                        <a href="/jogo1.html" class="btn btn-opt w-100">
+                                        <a href="jogo1.html" class="btn btn-opt w-100">
                                             Refazer teste
                                         </a>
                                     </div>    
-                                    <div class="col">
-                                        <a href="jogo2.html" class="btn btn-next w-100" data-next="game-card-${i + 1}">
-                                            Avançar
+                                    <!--<div class="col">
+                                        <a href="javascript:close_window();" class="btn w-100" data-next="game-card-${
+                                          i + 1
+                                        }">
+                                            Finalizar
                                         </a>
-                                    </div>    
+                                    </div>   
+                                    --> 
                                 </div>
                             </div>
                             `
-                    )
-                }
-            });
-        },
-        error: function (request, error) {
-            console.log(error);
-            alert(" Can't do because: " + error);
-        },
-    })
+          );
+        }
+      });
+    },
+    error: function (request, error) {
+      console.log(error);
+      alert(" Can't do because: " + error);
+    },
+  });
 }
 
-$(document).on("click", '.btn-next', function () {
-    var next = $(this).data('next');
-    $('.game-card').hide();
-    $('.' + next).show();
+$(document).on("click", ".btn-next", function () {
+  const next = $(this).data("next");
+  $(".game-card").hide();
+  $("." + next).show();
 });
 
-$(document).on("click", '.btn-opt', function () {
-    var index = $(this).data('index');
-    var resposta = $(this).data('value');
-    console.log(index, resposta);
-    if (resposta == questoes[index].verdade) {
-        acertos++;
-    }
-    if (index == questoes.length - 1) {
-        $('.game-card-' + (index + 1) + ' h4').html(
-            "VOCÊ COMPLETOU!<br><br>Pontuação: " + acertos + "/7"
-        )
-    }
-    $('.game-card').hide();
-    $('.game-card-' + index + '-result').show();
+$(document).on("click", ".btn-opt", function () {
+  var index = $(this).data("index");
+  var resposta = $(this).data("value");
+  console.log(index, resposta);
+  if (resposta == questoes[index].verdade) {
+    acertos++;
+    acertou = true;
+  } else {
+    acertou = false;
+  }
+  if (index == questoes.length - 1) {
+    $(".game-card-" + (index + 1) + " h4").html(
+      "VOCÊ COMPLETOU!<br><br>Pontuação: " + acertos + "/7"
+    );
+  }
+  $(".game-card").hide();
+  $(".game-card-" + index + "-result").show();
+});
+
+$(document).on("click", ".btn-back", function () {
+  const current = $(this).data("current");
+  $(".game-card").hide();
+  $("." + current).show();
+
+  if (!acertou) {
+    acertos--;
+  }
 });
--- scripts/jogo2.js
+++ scripts/jogo2.js
@@ -3,147 +3,175 @@ var grupos = [];
 var current_index = -1;
 
 function imprimirGrupo(index, letra) {
-    return ('<div data-index=' + index +
-        ' data-value=' + letra +
-        ' class="btn btn-grupo btn-grupo-' + letra + '">'
-        + letra + '</div>')
+  return (
+    "<div data-index=" +
+    index +
+    " data-value=" +
+    letra +
+    ' class="btn btn-grupo btn-grupo-' +
+    letra +
+    '">' +
+    letra +
+    "</div>"
+  );
 }
 
 function printQuestoes() {
-    $.ajax({
-        url: '../scripts/aedes.json',
-        dataType: "json",
-        type: 'GET',
-        success: function (_data) {
-            criadouros = _data[1].criadouros;
-            grupos = _data[1].grupos;
-            _data[1].grupos.forEach(function (value, i) {
-                var content = '';
-                for (let j = 0; j < value.quantidade; j++) {
-                    content += imprimirGrupo(i, value.letra)
-                }
-                $('.row-grupos').append(
-                    '<div class=" col-3 col-sm-6 col-grupo col-grupo-' + value.letra + '">'
-                    + content +
-                    '</div>'
-                )
-            })
-            $('.btn-grupo').draggable();
+  $.ajax({
+    url: "../scripts/aedes.json",
+    dataType: "json",
+    type: "GET",
+    success: function (_data) {
+      criadouros = _data[1].criadouros;
+      grupos = _data[1].grupos;
+      _data[1].grupos.forEach(function (value, i) {
+        var content = "";
+        for (let j = 0; j < value.quantidade; j++) {
+          content += imprimirGrupo(i, value.letra);
+        }
+        $(".row-grupos").append(
+          '<div class=" col-3 col-sm-6 col-grupo col-grupo-' +
+            value.letra +
+            '">' +
+            content +
+            "</div>"
+        );
+      });
+      $(".btn-grupo").draggable();
 
-            _data[1].criadouros.forEach(function (value, i) {
-                $('.row-criadouros').prepend(
-                    `
+      _data[1].criadouros.forEach(function (value, i) {
+        $(".row-criadouros").prepend(
+          `
                     <div class="col-6 col-sm-3 col-criadouro">
                         <div class="flex-criadouro d-flex flex-column justify-content-center">
-                            <div class="d-flex align-items-end mt-auto"><p class="text-center my-2 w-100">`
-                    + value.titulo +
-                    `
+                            <div class="d-flex align-items-end mt-auto"><p class="text-center my-2 w-100">` +
+            value.titulo +
+            `
                             </p></div>
                             <div class="close-container">
                             </div>
-                            <button data-index=`
-                    + i +
-                    ` class="input-criadouro" ></button>
+                            <button data-index=` +
+            i +
+            ` class="input-criadouro" ></button>
                         </div>
                     </div>
                     `
-                );
-            })
+        );
+      });
 
-            $('.input-criadouro').droppable({
-                accept: ".btn-grupo",
-                tolerance: "pointer",
-                drop: function (event, ui) {
-                    if ($(this).hasClass('ui-dropped')) {
-                        $('.col-grupo-' + $(this).val()).append(
-                            imprimirGrupo($(this).data('index'), $(this).val())
-                        )
-                        $('.btn-grupo-' + $(this).val()).draggable();
-                    }
-                    $(this).val(ui.draggable.data("value"));
-                    $(this).html(ui.draggable.data("value"));
-                    $(this).addClass('ui-dropped')
-                    ui.draggable.remove()
-                }
-            });
+      $(".input-criadouro").droppable({
+        accept: ".btn-grupo",
+        tolerance: "pointer",
+        drop: function (event, ui) {
+          if ($(this).hasClass("ui-dropped")) {
+            $(".col-grupo-" + $(this).val()).append(
+              imprimirGrupo($(this).data("index"), $(this).val())
+            );
+            $(".btn-grupo-" + $(this).val()).draggable();
+          }
+          $(this).val(ui.draggable.data("value"));
+          $(this).html(ui.draggable.data("value"));
+          $(this).addClass("ui-dropped");
+          ui.draggable.remove();
         },
-        error: function (request, error) {
-            console.log(error);
-            alert(" Can't do because: " + error);
-        },
-    })
+      });
+    },
+    error: function (request, error) {
+      console.log(error);
+      alert(" Can't do because: " + error);
+    },
+  });
 }
 
-
-$(document).on('click', '.close-container', function () {
-    var input = $(this).parent().find('input');
-    $('.col-grupo-' + input.val()).append(
-        imprimirGrupo(input.data('index'), input.val())
-    )
-    $('.btn-grupo-' + input.val()).draggable();
-    input.val('');
-    input.removeClass('ui-dropped');
-})
+$(document).on("click", ".close-container", function () {
+  var input = $(this).parent().find("input");
+  $(".col-grupo-" + input.val()).append(
+    imprimirGrupo(input.data("index"), input.val())
+  );
+  $(".btn-grupo-" + input.val()).draggable();
+  input.val("");
+  input.removeClass("ui-dropped");
+});
 
 printQuestoes();
 
-$(document).on('click', '.btn-grupo', function () {
-    current_index = $(this).data('index');
-    console.log(current_index)
-})
+$(document).on("click", ".btn-grupo", function () {
+  current_index = $(this).data("index");
+});
 
-$(document).on('click', '.input-criadouro', function () {
-    if ($(this).hasClass('ui-dropped')) {
-        $('.col-grupo-' + $(this).val()).append(
-            imprimirGrupo($(this).data('index'), $(this).val())
-        )
-        $('.btn-grupo-' + $(this).val()).draggable();
-    }
-    const current_btn = $('.btn-grupo[data-index= ' + current_index +']').eq(0);
-    console.log(current_btn);
+$(document).on("click", ".input-criadouro", function () {
+  if ($(this).hasClass("ui-dropped")) {
+    $(".col-grupo-" + $(this).val()).append(
+      imprimirGrupo($(this).data("index"), $(this).val())
+    );
+    $(".btn-grupo-" + $(this).val()).draggable();
+  }
+  if (current_index != -1) {
+    const current_btn = $(".btn-grupo[data-index= " + current_index + "]").eq(
+      0
+    );
     $(this).val(current_btn.data("value"));
     $(this).html(current_btn.data("value"));
-    $(this).addClass('ui-dropped')
-    current_btn.remove()
-})
+    $(this).addClass("ui-dropped");
+    current_btn.remove();
+    current_index = -1;
+  } else {
+    $(this).removeAttr("value");
+    $(this).html("");
+    $(this).removeClass("ui-dropped");
+  }
+});
 
-$(document).on('click', '.card-feedback .btn-back', function () {
-    $('.card-feedback').hide();
-    $('.card-criadouro').show();
-})
+$(document).on("click", ".card-feedback .btn-back", function () {
+  $(".card-feedback").hide();
+  $(".card-criadouro").show();
+});
 
-$(document).on('click', '.card-criadouro .btn-feedback', function () {
-    $('.card-criadouro').hide();
-    $('.card-feedback').show();
-})
+$(document).on("click", ".card-criadouro .btn-feedback", function () {
+  $(".card-criadouro").hide();
+  $(".card-feedback").show();
+});
 
-const success = new bootstrap.Modal('#successModal', {
-    backdrop: 'static',
-    keyboard: false
-})
+const success = new bootstrap.Modal("#successModal", {
+  backdrop: "static",
+  keyboard: false,
+});
 
-$(document).on('click', '.btn-verif', function () {
-    var acertos = 0;
-    if ($('.ui-dropped').length == criadouros.length) {
-        $('.ui-dropped').each(function (value, i) {
+$(document).on("click", ".btn-verif", function () {
+  var acertos = 0;
+  if ($(".ui-dropped").length == criadouros.length) {
+    $(".ui-dropped").each(function (value, i) {
+      var criadouro = criadouros[$(this).data("index")];
+      if (criadouro.resposta == $(this).val()) {
+        $(this).addClass("ui-right");
+        acertos++;
+      } else {
+        $(this).addClass("ui-wrong");
+      }
+    });
+    $(".modal .card-title").html(
+      `Você acertou ${acertos} de ${criadouros.length} criadouros.`
+    );
+    success.show();
+  }
+});
 
-            var criadouro = criadouros[$(this).data('index')]
-            if (criadouro.resposta == $(this).val()) {
-                $(this).addClass('ui-right');
-                acertos++;
-            }
-            else {
-                $(this).addClass('ui-wrong');
-            }
-        });
-        $('.modal p').html(
-            "Pontuação: " + acertos + "/" + criadouros.length
-        )
-        success.show();
-    }
+function refazerTeste() {
+  console.log($('.ui-wrong'));
+  $(".ui-wrong").each(function( index ) {
+    $(".col-grupo-" + $(this).val()).append(
+      imprimirGrupo($(this).data("index"), $(this).val())
+    );
+    $(".btn-grupo-" + $(this).val()).draggable();
+    $(this).removeAttr("value");
+    $(this).html("");
+    $(this).removeClass("ui-dropped");
+    $(this).removeClass("ui-wrong");
+  });
+  $('.ui-right').removeClass("ui-right");
+  success.hide();
+}
 
-    //$('.btn-verif').hide();
-    //$('.btn-retry').show();
-    //}
-
-})
+$(document).on("click", ".btn-redo", function () {
+  refazerTeste();
+});
